28b429eb50dc35d3b1841f8a8043766cffe153f3
Cesarjes99 <cesarjes1999@gmail.com>
2026-01-09 11:17:40 -0700

    Added test text to see footer in the correct position, changed the padding on the footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,28 @@ function App() {
   return (
     <div className="min-h-screen bg-background text-text font-[var(--font-raleway)]">
       <Header />
+      <section>
+        <h2>Hello World</h2>
+        <h1>Hello World</h1>
+        <p>This is a test</p>
+        <button>Click me</button>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+        <h2>Hello World</h2>
+      </section>
       <Footer />
     </div>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import logoSVG from "../../assets/logos/logo-svg.svg";
 function Footer() {
   return (
     <footer className="bg-[#1A1A1A] text-white">
-      <div className="max-w-7xl mx-auto px-6 py-16">
+      <div className="max-w-7xl mx-auto px-6 pt-15 pb-6">
 
         {/* Top */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
