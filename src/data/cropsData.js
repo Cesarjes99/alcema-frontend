9784b0c67694c25b8f6b95db5f422edc423f4a82
Cesarjes99 <cesarjes1999@gmail.com>
2026-01-23 12:10:07 -0700

    made crops data file

---
+++ src/data/cropsData.js
@@ -0,0 +1,67 @@
+import alfalfaImg from '../assets/pictures/fake-crops/alfalfa.webp'
+import cartamoImg from '../assets/pictures/fake-crops/cartamo.jpg'
+import celeryImg from '../assets/pictures/fake-crops/celery.jpg'
+import girasolImg from '../assets/pictures/fake-crops/girasol.jpg'
+import radicchioImg from '../assets/pictures/fake-crops/radicchio.webp'
+import trigoImg from '../assets/pictures/fake-crops/trigo.webp'
+import yellowSquashImg from '../assets/pictures/fake-crops/yellow-squash.jpg'
+import zucchiniImg from '../assets/pictures/fake-crops/zucchini.webp'
+
+export const crops = [
+  {
+    id: 1,
+    name: 'Alfalfa',
+    image: alfalfaImg,
+    description: 'Nuestro alfalfa es fresco, cremoso y cultivado responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  },
+  {
+    id: 2,
+    name: 'Apio',
+    image: celeryImg,
+    description: 'Nuestro apio es fresco, crujiente y cultivado responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  },
+  {
+    id: 3,
+    name: 'Radicchio',
+    image: radicchioImg,
+    description: 'Nuestro radicchio es fresco, jugoso y cultivado responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  },
+  {
+    id: 4,
+    name: 'Zucchini',
+    image: zucchiniImg,
+    description: 'Nuestra zucchini es fresca, tierna y cultivada responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  },
+  {
+    id: 5,
+    name: 'Yellow Squash',
+    image: yellowSquashImg,
+    description: 'Nuestra yellow squash es fresca, dulce y cultivada responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  },
+  {
+    id: 6,
+    name: 'Trigo',
+    image: trigoImg,
+    description: 'Nuestro trigo es de alta calidad, cultivado responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  },
+  {
+    id: 7,
+    name: 'Girasol',
+    image: girasolImg,
+    description: 'Nuestro girasol es cultivado responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  },
+  {
+    id: 8,
+    name: 'Cártamo',
+    image: cartamoImg,
+    description: 'Nuestro cártamo es cultivado responsablemente, garantizando máxima calidad.',
+    features: ['Producción sustentable', 'Calidad de exportación', 'Cultivo responsable']
+  }
+]
